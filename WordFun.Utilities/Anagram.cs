f117de1b5773f20648dcc16597b3c1ebf1299b62
kharshaw <keith.harshaw@gmail.com>
2019-04-11 19:12:59 -0400

    Completed Anagram class

--- WordFun.Utilities/Anagram.cs
+++ WordFun.Utilities/Anagram.cs
@@ -1,24 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Text.RegularExpressions;
 
 namespace WordFun.Utilities
 {
     public class Anagram : IAnagram
     {
-        public Anagram(bool ignoreCase)
-        {
+        private bool _ignoreCase;
 
-        }
 
         public Anagram()
         {
-
+            _ignoreCase = false;
         }
+
+        public Anagram(bool ignoreCase)
+        {
+            _ignoreCase = ignoreCase;
+        }
+
+        private string AlphabetizeString(string input)
+        {
+            var arr = input.ToCharArray();
+            Array.Sort(arr);
+
+            return new string(arr);
+        }
+
+        private bool VerifyAlphanumeric(string input)
+        {
+            var regex = new Regex(@"^[a-zA-Z0-9\s]*$");
+
+            return regex.IsMatch(input);
+        }
+
 
         public bool AreAnagrams(string firstWord, string secondWord)
         {
-            throw new NotImplementedException();
+            if (firstWord == null || secondWord == null)
+            {
+                throw new ArgumentNullException("strings must not be null");
+            }
+
+            if (!VerifyAlphanumeric(firstWord) || !VerifyAlphanumeric(secondWord))
+            {
+                throw new ArgumentException("strings must be alphanumeric");
+            }
+
+            if (_ignoreCase)
+            {
+                firstWord = firstWord.ToUpper();
+                secondWord = secondWord.ToUpper();
+            }
+
+            var firstWordArray = firstWord.ToCharArray();
+            var secondWordArray = secondWord.ToCharArray();
+
+
+            return AlphabetizeString(firstWord) == AlphabetizeString(secondWord);
         }
     }
 }
